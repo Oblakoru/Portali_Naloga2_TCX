678873227fd8de469ea319b2e1b563a7c5de0381
Benjamin <benjamin.jeran@gmail.com>
2023-10-22 23:48:10 +0200

    Konec brez grafov

---
+++ PodatkiClass.py
@@ -0,0 +1,22 @@
+class PodatkiClass():
+    def __init__(self, id, activity_type, distance, duration, calories, hr_avg, hr_max, hr_min, altitude_avg, altitude_max, altitude_min, ascent, descent, steps, num_hills, avg_altitude, avg_ascent, distance_hills, hills_share):
+        self.id = id
+        self.activity_type = activity_type
+        self.distance = distance
+        self.duration = duration
+        self.calories = calories
+        self.hr_avg = hr_avg
+        self.hr_max = hr_max
+        self.hr_min = hr_min
+        self.altitude_avg = altitude_avg
+        self.altitude_max = altitude_max
+        self.altitude_min = altitude_min
+        self.ascent = ascent
+        self.descent = descent
+        self.steps = steps
+        self.num_hills = num_hills
+        self.avg_altitude = avg_altitude
+        self.avg_ascent = avg_ascent
+        self.distance_hills = distance_hills
+        self.hills_share = hills_share
+
---
+++ main.py
@@ -0,0 +1,113 @@
+import csv
+import json
+import os
+from sport_activities_features import TCXFile
+from PodatkiClass import PodatkiClass
+from sport_activities_features.hill_identification import HillIdentification
+from sport_activities_features.topographic_features import TopographicFeatures
+import time
+
+
+start = time.time()
+
+tcx_file = TCXFile()
+stevecID = 0
+
+BikingList = []
+RunningList = []
+OtherList = []
+
+for root, dirs, files in os.walk("Sport5"):
+   for datoteka in files:
+
+       try:
+           celaPot = os.path.join(root, datoteka)
+
+           # Integralne
+           integral_metrics = tcx_file.extract_integral_metrics(celaPot)
+
+           # Hribi in doline
+           activity = tcx_file.read_one_file(celaPot)
+           Hill = HillIdentification(activity['altitudes'], 30)
+           Hill.identify_hills()
+           all_hills = Hill.return_hills()
+
+           # extract features from data
+           Top = TopographicFeatures(all_hills)
+           num_hills = Top.num_of_hills()
+           avg_altitude = Top.avg_altitude_of_hills(activity['altitudes'])
+           avg_ascent = Top.avg_ascent_of_hills(activity['altitudes'])
+           distance_hills = Top.distance_of_hills(activity['positions'])
+           hills_share = Top.share_of_hills(distance_hills, activity['total_distance'])
+
+           #print('num hills: ', num_hills)
+           #print('avg_altitude: ', avg_altitude)
+           #print('avg_ascent: ', avg_ascent)
+           #print('total distance: ', activity['total_distance'] / 1000)
+           #print('distance_hills: ', distance_hills)
+           #print('hills_share: ', hills_share)
+
+           print(stevecID)
+
+           podatki = PodatkiClass(stevecID, integral_metrics['activity_type'], integral_metrics['distance'],
+                                  integral_metrics['duration'], integral_metrics['calories'],
+                                  integral_metrics['hr_avg'], integral_metrics['hr_max'], integral_metrics['hr_min'],
+                                  integral_metrics['altitude_avg'], integral_metrics['altitude_max'],
+                                  integral_metrics['altitude_min'], integral_metrics['ascent'],
+                                  integral_metrics['descent'], integral_metrics['steps'], num_hills, avg_altitude, avg_ascent, distance_hills, hills_share)
+
+           if podatki.activity_type == "Biking":
+               BikingList.append(podatki)
+           elif podatki.activity_type == "Running":
+               RunningList.append(podatki)
+           else:
+               OtherList.append(podatki)
+           stevecID += 1
+       except:
+           print(f"Napaka pri branju datoteke {datoteka}")
+           continue
+
+
+
+listInDatoteka = {"bikingData.csv": BikingList, "runningData.csv": RunningList, "otherData.csv": OtherList}
+
+
+for datoteka, seznam in listInDatoteka.items():
+    with open(datoteka, 'w', newline='') as csvfile:
+      # Write the header row to the CSV file.
+      csv_writer = csv.writer(csvfile)
+      csv_writer.writerow(['id', 'activity_type', 'distance', 'duration', 'calories', 'hr_avg', 'hr_max', 'hr_min', 'altitude_avg', 'altitude_max', 'altitude_min', 'ascent', 'descent', 'steps',
+                           'num_hills', 'avg_altitude', 'avg_ascent', 'distance_hills', 'hills_share'])
+
+      # Iterate over the list of PodatkiClass objects and write each row to the CSV file.
+      for podatki_object in seznam:
+        csv_writer.writerow([podatki_object.id,
+                            podatki_object.activity_type,
+                            podatki_object.distance,
+                            podatki_object.duration,
+                            podatki_object.calories,
+                            podatki_object.hr_avg,
+                            podatki_object.hr_max,
+                            podatki_object.hr_min,
+                            podatki_object.altitude_avg,
+                            podatki_object.altitude_max,
+                            podatki_object.altitude_min,
+                            podatki_object.ascent,
+                            podatki_object.descent,
+                            podatki_object.steps,
+                            podatki_object.num_hills,
+                            podatki_object.avg_altitude,
+                            podatki_object.avg_ascent,
+                            podatki_object.distance_hills,
+                            podatki_object.hills_share])
+
+      csvfile.close()
+
+end = time.time()
+temp = end-start
+print(temp)
+hours = temp//3600
+temp = temp - 3600*hours
+minutes = temp//60
+seconds = temp - 60*minutes
+print('%d:%d:%d' %(hours,minutes,seconds))
