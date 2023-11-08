c099c79bb989c80cf86ad3a5a927262292d2fe90
Benjamin <benjamin.jeran@gmail.com>
2023-11-08 21:30:19 +0100

    KONEC ZARES ALI PA TUDI NE?

--- PandasUporaba.py
+++ PandasUporaba.py
@@ -8,7 +8,9 @@ def remove_extension(filename):
     return base
 
 # Točka 1
-df = pd.read_csv("runningDataPandas.csv")
+
+pot = "runningData.csv"
+df = pd.read_csv(pot)
 
 ##Describe vrze error
 numericniStoplci = df.select_dtypes(include=[float, int]).columns
@@ -31,46 +33,58 @@ for col in numericniStoplci:
 
 ##očka 4, 5 (main) in 6
 
-potDoDatotekeOdstranitev = "runningDataPandas.csv"
+#potDoDatotekeOdstranitev = "runningDataPandas.csv"
 
-dfOdtranitevStolpca = pd.read_csv(potDoDatotekeOdstranitev)
+#dfOdtranitevStolpca = pd.read_csv(potDoDatotekeOdstranitev)
 
 #Axis 0 pomen da gleda sam po vertikali
-steviloPraznih = dfOdtranitevStolpca.isna().sum(axis=0)
+steviloPraznih = df.isna().sum(axis=0)
 print(steviloPraznih)
 stolpecZaOdstranitev = steviloPraznih.idxmax()
 
 
 if steviloPraznih.max() != 0:
-    print(f"Odstranjen bo: {dfOdtranitevStolpca.isna().sum().idxmax()} z vrednostjo: {dfOdtranitevStolpca.isna().sum().max()}")
-    dfOdstranjevanje = dfOdtranitevStolpca.drop(columns=dfOdtranitevStolpca.isna().sum().idxmax())
-    dfOdstranjevanje.to_csv(f"{remove_extension(potDoDatotekeOdstranitev)}Odstranjeno.csv", index=False)
+    print(f"Odstranjen bo: {df.isna().sum().idxmax()} z vrednostjo: {df.isna().sum().max()}")
+    dfOdstranjevanje = df.drop(columns=df.isna().sum().idxmax())
+
+
+    ####TUle še daj na konc!
+    #dfOdstranjevanje.to_csv(f"{remove_extension(df)}Odstranjeno.csv", index=False)
 else:
     print("Odstranjen ne bo noben stolpec, saj ni manjkajočih podatkov!")
 
 
-
 #Točka 7
 
-potDoDatotekeNormalizacija = "runningDataPandas.csv"
+#potDoDatotekeNormalizacija = "runningDataPandas.csv"
 
-dfNormalizacija = pd.read_csv(potDoDatotekeNormalizacija)
+#dfNormalizacija = pd.read_csv(potDoDatotekeNormalizacija)
 
-dfNormalizacija["caloriesNormalized"] = (dfNormalizacija["calories"] - dfNormalizacija["calories"].min()) / (dfNormalizacija["calories"].max() - dfNormalizacija["calories"].min())
+df["caloriesNormalized"] = (df["calories"] - df["calories"].min()) / (df["calories"].max() - df["calories"].min())
 
-dfNormalizacija.to_csv(f"{remove_extension(potDoDatotekeNormalizacija)}Normalizirano.csv", index=False)
+df.to_csv(f"{remove_extension(pot)}Normalizirano.csv", index=False)
 
-print(dfNormalizacija)
+#print(dfNormalizacija)
 
-#Točka 9
+#Točka 8
 
-df = pd.read_csv("bikingDataPandas.csv")
-
-#x
+#df = pd.read_csv("bikingDataPandas.csv")
 
 bins = [0, 300, 500, 800, 1500]
-labels = ['Povrecen Trening', 'Dober Trening', 'Zelo dober Trening', 'Extremno dober trening']
+labels = ['Povprecen Trening', 'Dober Trening', 'Zelo dober Trening', 'Extremno dober trening']
 
 df['Diskretizacija'] = pd.cut(df['calories'], bins=bins, labels=labels, right=False)
 
-print(df)
+df.to_csv(f"{remove_extension(pot)}Final.csv", index=False)
+
+#Točka 9
+
+df = pd.read_csv("runningDataFinal.csv")
+
+numericniStoplci = df.select_dtypes(include=[object]).columns
+
+for col in numericniStoplci:
+    print(df[col].unique())
+
+
+
