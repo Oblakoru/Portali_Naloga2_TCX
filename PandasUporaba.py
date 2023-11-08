54cc854260eba6c686db3c8222c263e806005b3b
Benjamin <benjamin.jeran@gmail.com>
2023-11-08 19:54:25 +0100

    Pred koncem

--- PandasUporaba.py
+++ PandasUporaba.py
@@ -1,74 +1,69 @@
 import pandas as pd
 
-import seaborn as sns
 
-# Za preinovanje headerja! #header = ['id', 'activity_type', 'distance', 'duration', 'calories', 'hr_avg', 'hr_max',
-# 'hr_min', 'altitude_avg', 'altitude_max', 'altitude_min', 'ascent', 'descent', 'steps', #
-# 'num_hills', 'avg_altitude', 'avg_ascent', 'distance_hills', 'hills_share', 'number_of_intervalsPower',
-# 'min_durationPower', 'max_durationPower', 'avg_durationPower', 'min_distancePower', 'max_distancePower',
-# 'avg_distancePower', 'number_of_intervalsHeart', #                           'min_duration_intervalHeart',
-# 'max_duration_intervalHeart', 'avg_duration_intervalHeart', 'min_distance_intervalHeart',
-# 'max_distance_intervalHeart', 'avg_distance_intervalHeart', 'min_heartrate_intervalHeart',
-# 'max_heartrate_intervalHeart', 'avg_heartrate_intervalHeart'] # #dfHeader = pd.read_csv("otherDataPandas.csv",
-# header=None) #new_header = ['new', 'column', 'names'] #dfHeader.to_csv("otherDataPandasHeaders.csv", header=header,
-# index=False)
+# Točka 1
+df = pd.read_csv("runningDataPandas.csv")
 
+##Describe vrze error
+numericniStoplci = df.select_dtypes(include=[float, int]).columns
 
-#Točka 1
-df = pd.read_csv("bikingDataPandas.csv")
-
-# print(df.describe(include=[float, int]))
-
-##Točka 2 in 3
-columns_described = df.select_dtypes(include=[float, int]).columns
-
-for col in columns_described:
+for col in numericniStoplci:
     if col == "id":
         continue
-    column_mean = df[col].mean()
-    column_median = df[col].median()
-    column_max = df[col].max()
-    column_min = df[col].min()
+    povprecje = df[col].mean()
+    mediana = df[col].median()
+    maxVrednost = df[col].max()
+    minVrednost = df[col].min()
+
+    #Isna - missing or null values
     manjkajoci = df[col].isna().sum()
 
     print(
-        f"Column: {col}, Mean: {column_mean}, Median: {column_median}, Max: {column_max}, Min: {column_min} Stevilo manjkajocih: {manjkajoci}")
+        f"Column: {col}, povprecje: {povprecje}, Mediana: {mediana}, Max: {maxVrednost}, Min: {minVrednost} Stevilo manjkajocih: {manjkajoci}")
     print("--------------------------------------------------------------------------")
 
-##Točka 4? WtF
+
+# 4. ) S pomočjo knjižnice sport-activities-features identificirajte intervale (power in heartrate). (Namig: uporabite tudi metodo calculate_interval_statistics). Iz identificiranih podatkov ustvarite novo podatkovno množico, ki bo predstavljala podatke o obeh tipih intervalov na posamezni trening.
+# 5. ) Novo nastalo množico združite s tisto, ki ste jo ustvarili v prejšnji nalogi.
+# 6. ) Stolpec kateri ima največ manjkajočih podatkov odstranite.
+# 7. ) Normalizirajte poljuben stolpec po metodi min-max normalizacije,
+# 8.) Poljubni stolpec z numeričnimi podatki diskretizirajte (ustvarite vsaj 3 in največ 7 kategorij) (primer1, primer2, primer3).
+#
+# 9. ) Za vsak stolpec s kategoričnimi podatki izpišite vse kategorije.
+#
+# 10. ) Novo združeno in procesirano podatkovno množico zapišite v novo csv datoteko.
+
+##Točka 4, 5 (main) in 6
+
+potDoDatotekeOdstranitev = "runningDataPandas.csv"
+
+dfOdtranitevStolpca = pd.read_csv(potDoDatotekeOdstranitev)
+
+##Axis 0 pomen da gleda sam po vertikali
+steviloPraznih = df.isna().sum(axis=0)
 
 
-##Točka 5 in 6 - DONE v mainu
 
-# Točka 7
 
-dfKopija = pd.read_csv("dummy.csv")
+print(f"Odstranjen bo: {dfOdtranitevStolpca.isna().sum().idxmax()} z vrednostjo: {dfOdtranitevStolpca.isna().sum().max()}")
+dfOdstranjevanje = dfOdtranitevStolpca.drop(columns=dfOdtranitevStolpca.isna().sum().idxmax())
+dfOdstranjevanje.to_csv(f"{potDoDatotekeOdstranitev}Odstranjeno.csv", index=False)
 
-print(f"Odstranjen bo: {dfKopija.isna().sum().idxmax()} z vrednostjo: {dfKopija.isna().sum().max()}")
-dfOdstranjevanje = dfKopija.drop(columns=dfKopija.isna().sum().idxmax())
-dfOdstranjevanje.to_csv("dummyKopija.csv", index=False)
 
 #Točka 8
 
 df_min_max_scaled = dfKopija.copy()
 
-df_min_max_scaled["age"] = (df_min_max_scaled["age"] - df_min_max_scaled["age"].min()) / (df_min_max_scaled["age"].max() - df_min_max_scaled["age"].min())
+df_min_max_scaled["calories"] = (df_min_max_scaled["calories"] - df_min_max_scaled["calories"].min()) / (df_min_max_scaled["calories"].max() - df_min_max_scaled["calories"].min())
 
-# view normalized data
 print(df_min_max_scaled)
 
-
 #Točka 9
+df = pd.read_csv("runningDataPandasHeaders.csv")
 
+bins = [0, 300, 500, 800, 1500]
+labels = ['Povrecen Trening', 'Dober Trening', 'Zelo dober Trening', 'Extremno dober trening']
 
-
-df = pd.read_csv("dummy.csv")
-
-# Define the ranges and labels for BMI categories
-bins = [0, 18.5, 30, 60, 100]  # Ranges for underweight, normal, overweight, and obese
-labels = ['Mlad', 'Srednje mlad', 'Star', 'Zelo star']
-
-# Discretize the 'BMI' column into categories
-df['Diskretizacija'] = pd.cut(df['age'], bins=bins, labels=labels, right=False)
+df['Diskretizacija'] = pd.cut(df['calories'], bins=bins, labels=labels, right=False)
 
 print(df)
